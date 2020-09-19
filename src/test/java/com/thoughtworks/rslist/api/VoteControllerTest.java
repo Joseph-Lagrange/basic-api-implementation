5c6635fd1d25e6270cc72807f80635643f6e4e2e
wudibin <781721794@qq.com>
2020-09-19 22:53:44 +0800

    implement should get part vote record within time

--- src/test/java/com/thoughtworks/rslist/api/VoteControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/VoteControllerTest.java
@@ -118,4 +118,39 @@ public class VoteControllerTest {
                 .param("endTimeString", "2020-09-23"))
                 .andExpect(jsonPath("$", hasSize(0)));
     }
+
+    @Test
+    public void should_get_part_vote_record_within_time() throws Exception {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date time = simpleDateFormat.parse("2020-09-10");
+        for (int i = 0; i < 3; i++) {
+            VotePO votePO = VotePO.builder().user(userPO).rsEvent(rsEventPO)
+                    .localDateTime(time)
+                    .num(i + 1)
+                    .build();
+            voteRepository.save(votePO);
+        }
+        for (int i = 0; i < 8; i++) {
+            VotePO votePO = VotePO.builder().user(userPO).rsEvent(rsEventPO)
+                    .localDateTime(new Date())
+                    .num(i + 1)
+                    .build();
+            voteRepository.save(votePO);
+        }
+
+        mockMvc.perform(get("/voteRecords")
+                .param("startTimeString", "2020-09-18")
+                .param("endTimeString", "2020-09-20"))
+                .andExpect(jsonPath("$", hasSize(8)))
+                .andExpect(jsonPath("$[0].userId", is(userPO.getId())))
+                .andExpect(jsonPath("$[0].rsEventId", is(rsEventPO.getId())))
+                .andExpect(jsonPath("$[0].voteNum", is(1)))
+                .andExpect(jsonPath("$[1].voteNum", is(2)))
+                .andExpect(jsonPath("$[2].voteNum", is(3)))
+                .andExpect(jsonPath("$[3].voteNum", is(4)))
+                .andExpect(jsonPath("$[4].voteNum", is(5)))
+                .andExpect(jsonPath("$[5].voteNum", is(6)))
+                .andExpect(jsonPath("$[6].voteNum", is(7)))
+                .andExpect(jsonPath("$[7].voteNum", is(8)));
+    }
 }
